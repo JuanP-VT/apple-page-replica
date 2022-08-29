50854cbf4ce91ce3531918763c6ab18373b80320
JuanP-VT <cv.juanp@gmail.com>
2022-08-29 17:03:06 -0600

    ADD hover effects to nav component

--- src/components/Nav.Style.ts
+++ src/components/Nav.Style.ts
@@ -13,15 +13,24 @@ export const NavBox = styled.div`
   div {
     display: flex;
     gap: 40px;
+    cursor: pointer;
     p {
       color: #888;
+      &:hover {
+        color: white;
+      }
     }
   }
   img {
     height: 80%;
+    cursor: pointer;
   }
   svg {
     height: 40%;
     color: #888;
+    &:hover {
+      color: white;
+      cursor: pointer;
+    }
   }
 `;
