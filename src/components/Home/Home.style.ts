72dd2e6b202ef5b416944ba84c8338b9c32536ee
JuanP-VT <cv.juanp@gmail.com>
2022-08-30 10:56:41 -0600

    UPD home component

--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -2,5 +2,13 @@ import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
   display: flex;
-  margin-top: 70px;
+  margin-top: 60px;
+`;
+
+export const HomeMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  border: 1px solid green;
+  height: 600px;
 `;
--- src/components/Home/HomeMain.tsx
+++ src/components/Home/HomeMain.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { HomeMainContainer } from "./Home.style";
 
 function HomeMain() {
-  return <div>HomeMain</div>;
+  return (
+    <HomeMainContainer>
+      <h1>RAZER BASILISK V3 PRO</h1>
+      <h3>OUR MOST ADVANCED GAMING MOUSE YET</h3>
+      <div className="links">
+        <a
+          href="https://www.razer.com/gaming-mice/razer-basilisk-v3-pro"
+          onClick={() => {
+            alert(
+              "You are leaving this page replica!, now heading to Razer original site"
+            );
+          }}
+        >
+          Learn More
+        </a>
+        <a
+          href="https://www.razer.com/gaming-mice/razer-basilisk-v3-pro/select-your-model"
+          onClick={() => {
+            alert(
+              "You are leaving this page replica!, now heading to Razer original site"
+            );
+          }}
+        >
+          Buy
+        </a>
+      </div>
+    </HomeMainContainer>
+  );
 }
 
 export default HomeMain;
