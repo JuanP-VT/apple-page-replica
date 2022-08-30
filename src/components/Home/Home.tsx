4c52dff67675240fe38d871a4afbe453b1be5c63
JuanP-VT <cv.juanp@gmail.com>
2022-08-30 12:21:15 -0600

    UPD Home component content

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { HomeWrapper } from "./Home.style";
 import HomeMain from "./HomeMain";
 import HomeImageOne from "../../img/homeIMG/HomeOne.jpg";
+import HomeImageTwo from "../../img/homeIMG/HomeTwo.jpg";
+import HomeImageThree from "../../img/homeIMG/HomeThree.jpg";
+
 function Home() {
   return (
     <HomeWrapper>
@@ -16,18 +19,20 @@ function Home() {
           linkTwoHref="https://www.razer.com/gaming-mice/razer-basilisk-v3-pro/select-your-model"
         />
         <HomeMain
-          backgroundImg={HomeImageOne}
-          headerText=""
-          contentText=""
-          linkOneTextContent=""
-          linkOneHref=""
+          backgroundImg={HomeImageTwo}
+          headerText="LET THE GAME BEGIN"
+          contentText="LIMITED-TIME SPECIALS ON SELECTED RAZER BLADES"
+          linkOneTextContent="Shop Now"
+          linkOneHref="https://www.razer.com/campaigns/intel-gamer-days-2022"
         />
         <HomeMain
-          backgroundImg={HomeImageOne}
-          headerText=""
-          contentText=""
-          linkOneTextContent=""
-          linkOneHref=""
+          backgroundImg={HomeImageThree}
+          headerText="NEW RAZER BLADE 15"
+          contentText="WORLD'S FIRST 240Hz OLED LAPTOP"
+          linkOneTextContent="Learn More"
+          linkOneHref="https://www.razer.com/gaming-laptops/razer-blade"
+          linkTwoTextContent="Buy"
+          linkTwoHref="https://www.razer.com/gaming-laptops/razer-blade-15/razercare-bundles"
         />
       </section>
       <section></section>
