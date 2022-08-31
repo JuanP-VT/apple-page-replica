aa9e8d30fc59883852c5c0de712e2c8e0abeb2d7
JuanP-VT <cv.juanp@gmail.com>
2022-08-31 09:59:32 -0600

    UPD Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -27,8 +27,49 @@ function Footer() {
               linkEightText="Newsletter"
               linkEightHref="https://www.razer.com/newsletter"
             />
-            <FooterMenu label="test" />
-            <FooterMenu label="test" />
+            <FooterMenu
+              label="Explore"
+              linkOneText="Technology"
+              linkOneHref="https://www.razer.com/technology"
+              linkTwoText="Chroma RGB"
+              linkTwoHref="https://www.razer.com/chroma"
+              linkThreeText="Concepts"
+              linkThreeHref="https://www.razer.com/concepts"
+              linkFourText="Esports"
+              linkFourHref="https://www.razer.com/esports"
+              linkFiveText="Collabs"
+              linkFiveHref="https://www.razer.com/lifestyle/collabs"
+            />
+            <FooterMenu
+              label="Support"
+              linkOneText="Get Help"
+              linkOneHref="https://support.razer.com/?_gl=1*1rqv64*_ga*OTQ3MzIxOTkuMTY2MTc5NTcxMg..*_ga_3TRK53PM75*MTY2MTk1OTE1Ni4xMS4xLjE2NjE5NjExODIuNTMuMC4w&_ga=2.139684603.952502628.1661795712-94732199.1661795712"
+              linkTwoText="Registration & Warranty"
+              linkTwoHref="https://www.razer.com/product-registration"
+              linkThreeText="RazerStore Support"
+              linkThreeHref="https://www.razer.com/razerstore-support"
+              linkFourText="RazerCare"
+              linkFourHref="https://www.razer.com/razercare"
+              linkFiveText="Manage Razer ID"
+              linkFiveHref="https://razerid.razer.com/?_gl=1*l524wh*_ga*OTQ3MzIxOTkuMTY2MTc5NTcxMg..*_ga_3TRK53PM75*MTY2MTk1OTE1Ni4xMS4xLjE2NjE5NjExODUuNTAuMC4w&_ga=2.139684603.952502628.1661795712-94732199.1661795712"
+              linkSixText="Support Videos"
+              linkSixHref="https://www.youtube.com/channel/UCrG4K8-XFEK5FyCz_jVzV-A/videos"
+              linkSevenText="Accessibility Statement"
+              linkSevenHref="https://www.razer.com/legal/commitment-to-accessibility"
+            />
+            <FooterMenu
+              label="Company"
+              linkOneText="About Us"
+              linkOneHref="https://www.razer.com/about-razer"
+              linkTwoText="Carreer"
+              linkTwoHref="https://careers.razer.com/?_gl=1*2quuz7*_ga*OTQ3MzIxOTkuMTY2MTc5NTcxMg..*_ga_3TRK53PM75*MTY2MTk1OTE1Ni4xMS4xLjE2NjE5NjEzMTQuMjEuMC4w&_ga=2.130797127.952502628.1661795712-94732199.1661795712&__cf_chl_tk=1vWId6IAWqS7fZUNUntzJ0QopvLP6xE74elLXDjuNnM-1661961350-0-gaNycGzNDj0"
+              linkThreeText="Press Room"
+              linkThreeHref="https://press.razer.com/?_gl=1%2A2quuz7%2A_ga%2AOTQ3MzIxOTkuMTY2MTc5NTcxMg..%2A_ga_3TRK53PM75%2AMTY2MTk1OTE1Ni4xMS4xLjE2NjE5NjEzMTQuMjEuMC4w&_ga=2.130797127.952502628.1661795712-94732199.1661795712"
+              linkFourText="zVentures"
+              linkFourHref="https://www.zvntrs.com/"
+              linkFiveText="Contact Us"
+              linkFiveHref="https://www.razer.com/contact-us"
+            />
           </div>
           <div className="forGamers">gamers</div>
         </div>
