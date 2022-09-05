cfc27a3caa5d6e40f4b5c61d67cfdcf76975f64d
JuanP-VT <cv.juanp@gmail.com>
2022-09-05 10:07:02 -0600

    UPD PC component

--- src/components/PC/PC.style.ts
+++ src/components/PC/PC.style.ts
@@ -20,8 +20,34 @@ export const PCNavStyledLink = styled.a`
     color: #ffff;
     font-size: 12px;
     font-weight: 400;
+    white-space: nowrap;
     &:hover {
       color: #44d62c;
     }
+  }
+`;
+
+export const PCHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #111111;
+  .title {
+    padding-top: 30px;
+    font-size: 36px;
+    font-weight: 200;
+    color: #44d62c;
+    padding-bottom: 5px;
+    margin-bottom: 8px;
+    text-align: center;
+    max-width: 1000px;
+  }
+  .content {
+    font-size: 18px;
+    color: #999;
+    font-weight: 300;
+    max-width: 1000px;
+    line-height: 28px;
+    padding: 20px;
   }
 `;
--- src/components/PC/PC.tsx
+++ src/components/PC/PC.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { PCWrapper } from "./PC.style";
+import { PCHeader, PCWrapper } from "./PC.style";
 import PCNav from "./PCNav";
 
 function PC() {
   return (
     <PCWrapper>
       <PCNav />
+      <section>
+        <PCHeader>
+          <div className="title">LAPTOPS & DESKTOPS</div>
+          <div className="content">
+            Razer systems are crafted to provide the ultimate performance for
+            gaming and work. Enjoy desktop power made mobile with our gaming and
+            productivity laptops, or go all-out for your build as you assemble
+            the most powerful aRGB Chroma powered battlestation.
+          </div>
+        </PCHeader>
+      </section>
     </PCWrapper>
   );
 }
