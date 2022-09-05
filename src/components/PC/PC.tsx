aa7031f77a9c65a7cbc739f75929ac765e586c6d
JuanP-VT <cv.juanp@gmail.com>
2022-09-05 17:01:26 -0600

    ADD img for PC

--- src/components/PC/PC.tsx
+++ src/components/PC/PC.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { PCWrapper } from "./PC.style";
 import PCMain from "./PCMain";
 import PCNav from "./PCNav";
-
+import imgOne from "../../img/pcIMG/razer-blade-15-2022-laptop-500x500.jpg";
+import imgTwo from "../../img/pcIMG/desktops-components-category--500x500.jpg";
+import imgThree from "../../img/pcIMG/accessories-category-500x500.jpg";
 function PC() {
   return (
     <PCWrapper>
@@ -13,17 +15,17 @@ function PC() {
         and work. Enjoy desktop power made mobile with our gaming and
         productivity laptops, or go all-out for your build as you assemble the
         most powerful aRGB Chroma powered battlestation"
-        imgSrc1="https://assets2.razerzone.com/images/pnx.assets/381e915d58d2b9759725c30a9f2c3a0f/razer-blade-15-2022-laptop-500x500.jpg"
+        imgSrc1={imgOne}
         titleText1="LAPTOP"
         descriptionText1="Sleek high-performance gaming laptops"
         linkHref1="https://www.razer.com/pc/gaming-laptops"
         titleText2="DESKTOPS & COMPONENTS"
-        imgSrc2="https://assets2.razerzone.com/images/pnx.assets/381e915d58d2b9759725c30a9f2c3a0f/desktops-components-category--500x500.jpg"
+        imgSrc2={imgTwo}
         descriptionText2="Engineered for enthusiasts and designed for performance"
         linkHref2="https://www.razer.com/pc/gaming-desktops-and-components"
         titleText3="ACCESSORIES"
         descriptionText3="Crafting the perfect gaming experience"
-        imgSrc3="https://assets2.razerzone.com/images/pnx.assets/381e915d58d2b9759725c30a9f2c3a0f/accessories-category-500x500.jpg"
+        imgSrc3={imgThree}
         linkHref3="https://www.razer.com/pc/gaming-pc-accessories"
       />
     </PCWrapper>
