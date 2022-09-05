60ba432dba31f96e688478d2aa9e54c98d32f8f8
JuanP-VT <cv.juanp@gmail.com>
2022-09-05 09:44:58 -0600

    UPD PCNav content

--- src/components/PC/PCNav.tsx
+++ src/components/PC/PCNav.tsx
@@ -5,18 +5,54 @@ import PCNavLink from "./PCNavLink";
 function PCNav() {
   return (
     <PCNavWrapper>
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
-      <PCNavLink />
+      <PCNavLink
+        linkText="Laptops"
+        linkHref="https://www.razer.com/pc/gaming-laptops"
+      />
+      <PCNavLink
+        linkText="Desktops & Components"
+        linkHref="https://www.razer.com/pc/gaming-desktops-and-components"
+      />
+      <PCNavLink
+        linkText="Monitors"
+        linkHref="https://www.razer.com/gaming/monitors/razer-raptor-27-165hz"
+      />
+      <PCNavLink
+        linkText="Mice"
+        linkHref="https://www.razer.com/pc/gaming-mice"
+      />
+      <PCNavLink
+        linkText="Mats"
+        linkHref="https://www.razer.com/pc/gaming-mouse-mats"
+      />
+      <PCNavLink
+        linkText="Keyboards"
+        linkHref="https://www.razer.com/pc/gaming-keyboards-and-keypads"
+      />
+      <PCNavLink
+        linkText="Headsets"
+        linkHref="https://www.razer.com/pc/gaming-headsets-and-audio"
+      />
+      <PCNavLink
+        linkText="Speakers"
+        linkHref="https://www.razer.com/pc/gaming-speakers"
+      />
+      <PCNavLink
+        linkText="Streaming"
+        linkHref="https://www.razer.com/pc/streaming"
+      />
+      <PCNavLink
+        linkText="Productivity"
+        linkHref="https://www.razer.com/productivity"
+      />
+      <PCNavLink
+        linkText="Chairs"
+        linkHref="https://www.razer.com/lifestyle/gaming-chairs"
+      />
+      <PCNavLink
+        linkText="Software"
+        linkHref="https://www.razer.com/pc/software"
+      />
     </PCNavWrapper>
   );
 }
--- src/components/PC/PCNavLink.tsx
+++ src/components/PC/PCNavLink.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { PCNavStyledLink } from "./PC.style";
-
-function PCNavLink() {
+type Props = {
+  linkText: string;
+  linkHref: string;
+};
+function PCNavLink({ linkHref, linkText }: Props) {
   return (
     <PCNavStyledLink
+      href={linkHref}
       onClick={() => {
         alert(
           "You are leaving this page replica!, now heading to Razer original site"
         );
       }}
     >
-      PCNavLink
+      <p>{linkText}</p>
     </PCNavStyledLink>
   );
 }
